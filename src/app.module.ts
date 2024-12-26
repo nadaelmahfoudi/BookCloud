2b913c064b07a6ed143e62c8c098eb745be8f722
Nada Elmahfoudi <elmahfoudinada17@gmail.com>
2024-12-26 12:21:24 +0100

    BK22-6 deleting duplicated folders

--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { BooksModule } from './books/books.module';
-import { DynamoDBModule } from './config/aws-dynamodb.module'; 
-
-@Module({
-  imports: [ConfigModule.forRoot(), DynamoDBModule, BooksModule], 
-})
-export class AppModule {}
--- src/config/aws-dynamodb.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DynamoDBService } from './aws-dynamodb.config';
-
-@Module({
-  providers: [DynamoDBService],
-  exports: [DynamoDBService], 
-})
-export class DynamoDBModule {}
