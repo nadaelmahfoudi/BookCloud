105964bb9a74613ef56a669c6f207242ab3b9a00
Nada Elmahfoudi <elmahfoudinada17@gmail.com>
2024-12-30 15:16:15 +0100

    Update project structure

--- frontend-book-cloud/src/App.jsx
+++ frontend-book-cloud/src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './components/NavBar';
 import HomePage from './pages/Home';
 import ProfilePage from './pages/Profile';
 import DashboardPage from './pages/Dashboard';
+import CreateBook from './pages/Books/AddBook';
 import Footer from './components/Footer';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/create-book" element={<CreateBook />} />
       </Routes>
       <Footer /> {/* Add Footer at the bottom */}
     </Router>
